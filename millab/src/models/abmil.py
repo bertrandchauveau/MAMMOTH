8cc5ad0a4c9913256b639ade83e91d0070538e76
Bertrand Chauveau <110421330+bertrandchauveau@users.noreply.github.com>
2026-06-27 19:41:10 +0200

    Fix import path for Mammoth module

--- millab/src/models/abmil.py
+++ millab/src/models/abmil.py
@@ -2,7 +2,7 @@ from src.models.mil_template import MIL
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from src.mammoth import Mammoth
+from mammoth import Mammoth
 from src.models.layers import GlobalAttention, GlobalGatedAttention, create_mlp
 from transformers import PretrainedConfig, PreTrainedModel
 from transformers import AutoConfig, AutoModel
